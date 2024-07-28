7043c9af42061c0290b46f3b9146d541126f0296
mukitul <mukitul.exabyting@bkash.com>
2024-07-28 13:03:07 +0600

    ...

--- src/main/java/com/mukit/springbootscheduler/config/DynamoDBConfig.java
+++ src/main/java/com/mukit/springbootscheduler/config/DynamoDBConfig.java
@@ -5,16 +5,19 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDynamoDBRepositories(basePackages = "com.mukit.springbootscheduler.repository")
 public class DynamoDBConfig {
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "us-west-2"))
+                .withEndpointConfiguration(new AwsClientBuilder
+                        .EndpointConfiguration("http://localhost:4566", "us-west-2"))
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("test", "test")))
                 .build();
     }
